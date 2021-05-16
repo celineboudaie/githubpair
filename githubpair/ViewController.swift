b029b0f76ebc489922c0a35a7207abe95cce3a47
celine <cboudaie@gmail.com>
2021-05-16 10:24:11 -0700

    Initial Commit

---
+++ githubpair/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubpair
+//
+//  Created by Celine Boudaie on 5/16/21.
+//  Copyright © 2021 Celine Boudaie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ githubpairTests/githubpairTests.swift
@@ -0,0 +1,34 @@
+//
+//  githubpairTests.swift
+//  githubpairTests
+//
+//  Created by Celine Boudaie on 5/16/21.
+//  Copyright © 2021 Celine Boudaie. All rights reserved.
+//
+
+import XCTest
+@testable import githubpair
+
+class githubpairTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
